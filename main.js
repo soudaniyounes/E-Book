afd88422bcf65ecc0be6021d34dff14cbf4a89ef
Younes-Soudani <soudaniyounes003@gmail.com>
2022-12-19 22:37:12 +0100

    the new reload

---
+++ main.js
@@ -0,0 +1,7 @@
+ const tooltip=document.querySelectorAll(".toolT");
+tooltip.forEach(t=>{
+    new bootstrap.Tooltip(t);
+}); 
+// let navbar = doucument.querySelector(".navbar-toggler");
+// let nav = doucument.querySelector(".navbar-nav");
+// nav.classList.add("d-none");
